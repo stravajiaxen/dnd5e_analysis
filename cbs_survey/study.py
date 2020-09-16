8ede40c0bf3d0de3a71c253de4eb4f80e193adcd
Matthew DeMartino <demartino.mcd@gmail.com>
2020-09-16 18:45:55 -0400

    [WIP] Adding Google Sheets API support while PyCharm is screwing around and being slow

--- cbs_survey/study.py
+++ cbs_survey/study.py
@@ -1,4 +1,8 @@
 import os
+import pickle
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
 import pandas as pd
 
 from .entry import Entry
@@ -13,7 +17,10 @@ class Study:
     """
 
     sources = os.path.join(os.path.dirname(os.path.realpath(__file__)), "sources")
+    secrets = os.path.join(os.path.dirname(os.path.realpath(__file__)), "secrets")
     partial_survey = os.path.join(sources, "sept15_partial_survey.xlsx")
+    gsheets_credentials = os.path.join(secrets, "credentials.json")
+    token = os.path.join(secrets, "token.pickle")
     mappings = {
         "Timestamp": "Timestamp",
         "What is your age?": "Player Age",
@@ -66,11 +73,15 @@ class Study:
             self.df = df
 
     @classmethod
-    def from_file(cls, fname):
+    def from_full_df(cls, df):
         """
-        Creates a study given the filename
+        Creates a study given the full dataframe
+
+        :param df: The Dataframe
+        :type df: Dataframe
+        :return: A Study
+        :rtype: Study
         """
-        df = pd.read_excel(fname)
         df = df.rename(mapper=cls.mappings, axis=1)
         entries = []
         for i, _ in enumerate(df.iloc):
@@ -81,5 +92,68 @@ class Study:
             entries.append(entry)
         return cls(entries, df)
 
+    @classmethod
+    def from_file(cls, fname):
+        """
+        Creates a study given the filename
+        """
+        df = pd.read_excel(fname)
+        return cls.from_full_df(df)
 
-study = Study.from_file(Study.partial_survey)
+    @classmethod
+    def from_latest(cls):
+        """
+        Get the latest study from Google
+
+        :return: The latest study
+        :rtype: Study
+        """
+        # Pulls data from the entire spreadsheet tab.
+        RANGE_NAME = 'spreadsheet_tab_name!'
+
+        # Pulls data only from the specified range of cells.
+        RANGE_NAME = 'spreadsheet_tab_name!A2:C6'
+
+        SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+        SPREADSHEET_ID = '1sj_9P_u6sxakhzdEpb1LF-rlMnyHWO0gHWH4tr2j3M4'
+        RANGE_NAME = 'Responses!'
+        data = pull_sheet_data(SCOPES, SPREADSHEET_ID, RANGE_NAME)
+        df = pd.DataFrame(data[1:], columns=data[0])
+        return cls.from_full_df(df)
+
+
+def pull_sheet_data(SCOPES, SPREADSHEET_ID, RANGE_NAME):
+    creds = gsheet_api_check(SCOPES)
+    service = build('sheets', 'v4', credentials=creds)
+    sheet = service.spreadsheets()
+    result = sheet.values().get(
+        spreadsheetId=SPREADSHEET_ID,
+        range=RANGE_NAME).execute()
+    values = result.get('values', [])
+    if not values:
+        raise ValueError("Please use a valid sheet")
+    else:
+        rows = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                  range=RANGE_NAME).execute()
+        data = rows.get('values')
+        return data
+
+def gsheet_api_check(SCOPES):
+    creds = None
+    if os.path.exists(Study.token):
+        with open(Study.token, 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                Study.gsheets_credentials, SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open(Study.token, 'wb') as token:
+            pickle.dump(creds, token)
+    return creds
+
+
+partial_study = Study.from_file(Study.partial_survey)
+latest_study = Study.from_latest()
